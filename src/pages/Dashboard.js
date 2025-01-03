646d7f649e1256c9074f543ca6c96b0b2248cf61
Nghia Nguyen <nguyenn2001@gmail.com>
2025-01-03 05:17:17 -0500

    Update Dashboard.js

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -5,25 +5,39 @@ import { AuthContext } from '../provider/Authentication'
 import { useNavigate } from 'react-router-dom'
 
 export default function Dashboard() {
-    const { currentUser } = useContext(AuthContext);
+    const displayFixingMessage = true;
+    const { currentUser, signOutUser } = useContext(AuthContext);
     const navigate = useNavigate();
 
     // useEffect ensures that the component is mounted before the navigation is complete.
     // otherwise it would mount the component without navigating
     useEffect(() => {
+        const handleSignOutUserAndDashboard = () => {
+            signOutUser()
+            navigate('/dashboard')
+        }
+
+        if (displayFixingMessage) {
+            handleSignOutUserAndDashboard()
+        }
         if (currentUser && !currentUser.emailVerified) {
             navigate('/sign-up/verify');
         }
-    }, [currentUser, navigate]);
+    }, [currentUser, navigate, displayFixingMessage, signOutUser]);
 
-    if (currentUser) {
-        if (currentUser.emailVerified) {
-            return <DashboardSignedInState />;
-        } else {
-            return null;
-        }
 
+    if (displayFixingMessage) {
+        return <h1>Apologizes :/ this site is undergoing some changes, please return at a later date...</h1>
     } else {
-        return <DashboardGuestState />;
+        if (currentUser) {
+            if (currentUser.emailVerified) {
+                return <DashboardSignedInState />;
+            } else {
+                return null;
+            }
+
+        } else {
+            return <DashboardGuestState />;
+        }
     }
 }
